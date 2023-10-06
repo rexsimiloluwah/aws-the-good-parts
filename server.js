767a7f2a4839c94e37c2a28054690021d122e5fc
rexsimiloluwah <adetoyosi2001@gmail.com>
2023-10-06 04:33:49 +0100

    Added stack name to app

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const { hostname } = require("os")
 const http = require("http")
 
 const PORT = process.env.PORT || 8080 
+const STACK_NAME = process.env.STACK_NAME || "Unknown Stack"
 
-message = `<h1>Hello, My people. From ${hostname()}</h1>`
+message = `<h1>Hello, My people. From ${hostname()}</h1> <p>From stack ${STACK_NAME}`
 
 const server = http.createServer((_, res) => {
     res.statusCode = 200;
